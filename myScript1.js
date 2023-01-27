a182c0278a99d6566e24fb31aa1248b05aa5fe1f
Priscilla Alexis Mitchell <alexismitchell42@gmail.com>
2023-01-26 19:04:24 -0600

    Updated index.html_myscript1&2 jan26 7:04pm

---
+++ myScript1.js
@@ -0,0 +1,30 @@
+function myFunction() {
+    document.getElementById("math").innerHTML = 11+12 ;
+  } //*This is a CODE BLOCK within a function
+
+    window.alert (8*9)
+   //running an alert 
+  
+let firstname= "Priscilla"
+let lastname=" Mitchell"
+//This is a global
+
+   document.getElementById("myname").innerHTML = 'Priscilla Mitchell'; 
+ // Change answer when button is clicked:
+ //changing property of dom element
+ // element includes "strings"
+//outputing to the console log
+console.log(document.getElementById("myname"));
+ 
+{
+    let x, y;
+  x = 5;
+  y = 6;
+  document.getElementById("letv").innerHTML = x * y;
+} //* this is a block variable
+  
+const person= {
+    name: "John Doe",
+    age: 46,
+    ethnicity: "Latino"
+} //Const variable and the value is an object
---
+++ myScript2.js
@@ -0,0 +1,5 @@
+let score=0;
+
+function myFunction(){
+    let score=0
+}
